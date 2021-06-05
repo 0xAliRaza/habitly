020e4e63b756978e630cca8e70dde0282de26b59
Ali Raza <artanha7@gmail.com>
2021-06-05 08:52:49 +0500

    Add bootstrap

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,4 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-
   <div>
     <button
       class="btn btn-primary btn-margin"
@@ -29,34 +24,11 @@
       Log Out
     </button>
     {{ message }}
-    <br />
   </div>
-  <hr />
-
-  <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '~bootstrap/scss/bootstrap.scss';
 </style>
 
 <script>
