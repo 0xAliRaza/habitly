755cc096631788d4ad9cf3e7d1b46f5a5f109c8d
Ali Raza <artanha7@gmail.com>
2021-06-05 09:48:30 +0500

    Add Habit model and migration

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Habit(models.Model):
+    user_id = models.UUIDField()
+    title = models.CharField(max_length=50)
+    ritual = models.CharField(max_length=50, blank=True)
+    description = models.CharField(max_length=250, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
